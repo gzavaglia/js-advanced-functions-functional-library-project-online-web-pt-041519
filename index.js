e7938e30f776291af9e7e41c50e90f6f78ef4a9c
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-11-29 19:26:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fi = (function() {
     each: function(collection, callback) {
       
       for (let i = 0; i < collection.length; i++)
-        callback(collection[i])
+        {callback(collection[i])}
 
       
       return collection
